03ca4c6e7bcf6899dc28dc71842042b17860cc76
tajtu1406 <47653614+tajtu1406@users.noreply.github.com>
2019-04-05 14:51:23 +0700

    Update BT_A3.cpp

--- BT_A3.cpp
+++ BT_A3.cpp
@@ -11,4 +11,7 @@ int main(){
     cerr <<"c: " << "-" << c << "-" << endl;
     delete c;
     cerr << "a after deleting c:" << "-" << a << "-" << endl;
+    //    Vì lệnh delete chỉ áp dụng lên con trỏ khởi tao theo kiểu bộ nhớ đông
+    //    Vì vậy khi sử dụng lệnh này vs con
+    //    trỏ khởi tạo thông thường lệnh này sẽ không có hiệu lực
 }
